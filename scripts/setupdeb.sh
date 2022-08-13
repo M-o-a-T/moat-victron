c5c7c0a0e255b133d5a848b47dc0e7378316839e
Matthias Urlichs <matthias@urlichs.de>
2022-08-13 22:17:32 +0200

    btrfs-progs isn't in OPKG
    so call it from the Debian partition

---
+++ scripts/setupdeb.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if ! test -d /opt/debian/bin ; then
+	echo "This script finalizes your Debian-plus-Victron installation." >&2
+	echo "You need to run it on Venus." >&2
+	exit 1
+fi
+
+mkdir -p /usr/local/bin
+mkdir -p /usr/local/sbin
+
+chroot /opt/debian apt-get update
+chroot /opt/debian apt-get install btrfs-progs
+cat >/usr/local/bin/btrfs <<'_'
+
+#!/bin/sh
+
+exec env PATH=/opt/debian/bin LD_LIBRARY_PATH=/opt/debian/lib/aarch64-linux-gnu/ \
+    /opt/debian/bin/$(basename $0) \
+    "$@"
+_
+
+chmod +x /usr/local/bin/btrfs
